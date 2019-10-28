94f9969bf05a0bd7b38d4134cc512b09b80f92c4
danitab85 <danielaballesteros85@gmail.com>
2019-10-27 21:17:54 -0300

    Vista y controlador schools

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'schools/index'
   get 'users/show'
-  
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :products, only: [:index, :show] do
