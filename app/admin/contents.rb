6df605e2d476b2846abb98ab233868fc0ed0bcac
danitab85 <danielaballesteros85@gmail.com>
2019-10-29 16:16:20 -0300

    Video de content, amazonS3

--- app/admin/contents.rb
+++ app/admin/contents.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Content do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :description, :product_id
+   permit_params :name, :description, :product_id, :image
   #
   # or
   #
@@ -14,5 +14,12 @@ ActiveAdmin.register Content do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+  form do |f|
+     f.semantic_errors # Muestra errores en el formulario
+     f.input :image, as: :file
+     f.input :product_id
+     f.input :name # Construye un input para cada atributo del modelo
+     f.input :description
+     f.actions # Agrega los botones Submit y Cancel
+  end
 end
--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -3,7 +3,7 @@ class ContentsController < InheritedResources::Base
   private
 
     def content_params
-      params.require(:content).permit(:name, :description, :product_id)
+      params.require(:content).permit(:name, :description, :product_id, :image)
     end
 
 end
