de9b45ee255feddaf659b8e30120c79fb7a9c0eb
danitab85 <danielaballesteros85@gmail.com>
2019-10-30 14:00:31 -0300

    validates user email unico

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
 
   has_many :orders
   has_many :products, through: :orders
+  validates :email, uniqueness: true
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
